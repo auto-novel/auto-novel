905feefe4db98d1bffcf1618cc4ca8096b248fdb
PipeYume <1178795412@qq.com>
2026-04-24 17:10:23 +0800

    更新接口定义
    Update types.ts

--- packages/translation/src/index.ts
+++
@@ -1,8 +0,0 @@
-export type {
-  TranslationTask,
-  Translator,
-  EngineCallbacks,
-  TranslationResult,
-} from './types';
-export { OpenAiTranslator, type OpenAiTranslatorConfig } from './translators';
-export { TranslationEngine } from './engine';
